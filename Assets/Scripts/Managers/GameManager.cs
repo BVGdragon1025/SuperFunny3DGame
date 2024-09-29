0778abb1e01f9b076850bb616ae4f3fe3d4b1519
Dominik Nawrot <dnawrot1025@gmail.com>
2024-09-29 02:00:08 +0200

    Lemur adding

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -34,6 +35,7 @@ public class GameManager : MonoBehaviour
         }
 
         InvokeRepeating(nameof(AddLemur), _lemurSpawnTimer, _lemurSpawnTimer);
+        _unemployedLemurs = _lemurAmount;
     }
 
     public void ChangeResourcesAmount(ResourceType resourceType, float resourceAmount)
@@ -78,6 +80,7 @@ public class GameManager : MonoBehaviour
     private void AddLemur()
     {
         _lemurAmount++;
+        _unemployedLemurs++;
         Debug.Log($"Lemur Added! Current amount: {_lemurAmount}");
 
     }
